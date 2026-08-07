6dd066de4ec9dcb268a486164590cac9ed8da7dc
jusgowiturs <prabhuramn4186@gmail.com>
2026-08-07 12:22:09 +0000

    LED Blinking Make file updated

--- TANG_NANO_4k/LEDBlinking/src/counter.v
+++ TANG_NANO_4k/LEDBlinking/src/counter.v
@@ -1,26 +1,26 @@
-module counter (
-    input   sys_clk,
-    input   sys_rst_n,     // reset input
-    output  wire led        // LED
-);
+    module counter (
+        input   sys_clk,
+        input   sys_rst_n,     // reset input
+        output  wire led        // LED
+    );
 
-reg [31:0] counter;        
-reg  ledreg;
-always @(posedge sys_clk or negedge sys_rst_n) begin
-    if (!sys_rst_n) begin
-        counter <= 32'd0;
-        ledreg  <= 1'b0;
-    end
-    else begin
-        if (counter == 32'd1000000-2) begin
-            counter <= 32'd0;
-            ledreg <= ~ledreg;   // Toggle LED
+    reg [3:0] counter;        
+    reg  ledreg;
+    always @(posedge sys_clk or negedge sys_rst_n) begin
+        if (!sys_rst_n) begin
+            counter <= 4'd0;
+            ledreg  <= 1'b0;
         end
         else begin
-            counter <= counter + 1'b1;
+            if (counter == 4'd0-2) begin
+                counter <= 4'd0;
+                ledreg <= ~ledreg;   // Toggle LED
+            end
+            else begin
+                counter <= counter + 1'b1;
+            end
         end
     end
-end
 
-assign led = ledreg;
-endmodule
+    assign led = ledreg;
+    endmodule
